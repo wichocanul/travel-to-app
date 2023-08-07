8dfd0d61a62955132bebafb109114fad78f99d29
wicho <wichocanul07@gmail.com>
2023-08-07 11:50:03 -0600

    POV: improve markers in map section home

--- src/app/client-app/pages/home/map/map.component.ts
+++ src/app/client-app/pages/home/map/map.component.ts
@@ -12,79 +12,111 @@ export class MapComponent {
 
   map: L.Map = {} as L.Map;
   markersRest: L.LayerGroup = L.layerGroup();
+  globalMarkers: PlaceData[] = [];
 
   constructor( private mapService: MapService ) {}
 
-  ngAfterViewInit() {
+  async ngAfterViewInit() {
 
-    this.map = new L.Map('map').setView([19.8391, -98.0301], 15);
+    try {
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy;',
-      maxZoom: 20
-    }).addTo(this.map);
+      await this.createMap();
+      await this.getMarkers();
+      await this.setMarkers();
 
-    this.getMarkers();
+    } catch (error) {
+      console.error('Ocurrió un error', error);
+      alert('No se cargó correctamente');
+    }
 
   }
 
-  getMarkers() {
-    this.mapService.getAllPlaces()
-      .subscribe({
-        next: (resp) => {
-          this.setMarkers(resp);
-        },
-        error: (err) => {
-          console.log(err);
-        }
-      })
-  }
+  async createMap() {
 
-  setMarkers(resp:PlaceData[]) {
-    this.markersRest.clearLayers();
+    return new Promise<void>((resolve, reject) => {
 
-    resp.forEach((data: any) => {
-      const lat = parseFloat(data.coordinates.lat);
-      const lng = parseFloat(data.coordinates.lng);
-      const icon = L.icon({
-        iconUrl: `./assets/imgs/map/${data.type}.png`,
-        iconSize: [32, 32],
-        iconAnchor: [16, 32],
-        popupAnchor: [0, -32]
-      });
-      const marker = L.marker([lat, lng], {icon});
-      let popupContent = '';
-      const introContent = `
-        <h3 style="text-align: center; margin-bottom: 10px">${data.name}</h3>
-        <p style="text-align: justify; width: 100%;">${data.description}</p>
-        <div style="width: 100%; height:70px; display:flex; gap:5px; justify-content: center; align-items: center;">
-          <div style="width: 100px;">
-            <img style="width:100%" src="https://www.corazondepuebla.com.mx/wp-content/uploads/2018/10/header-chignahuapan-puebla-mexico-pueblos-magicos.jpg">
-          </div>
-          <div style="width: 100px;">
-            <img style="width:100%" src="https://mexicodesconocidoviajes.mx/wp-content/uploads/2018/09/Puebla-Chignahuapan-festival-luz-y-Vida-DPG_IMG_2006.jpg">
-          </div>
-        </div>`;
+      this.map = new L.Map('map').setView([19.8391, -98.0301], 15);
 
-        const eventContent = `
-        <div style="margin:10px 0px;">
-          <p><span style="font-weight: bold;">Evento:</span> ${data.nameEvent}</p>
-          <p><span style="font-weight: bold;">Fecha: </span>${data.dayEvent} <span style="font-weight: bold;">Hora: </span>${data.hourEvent}</p>
-        </div>`;
+      L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy;',
+        maxZoom: 20
+      }).addTo(this.map);
 
-        const footerContent = `
-        <div style="display:flex; gap:5px; justify-content: center; align-items: center; margin-bottom:10px; margin-top:10px;">
-          <button style="width:auto; padding: 5px 10px; cursor:pointer; background: #009CDE;color:#FFF; border-radius:3px;">Ver lugar</button>
-          <button style="width:auto; padding: 5px 10px; cursor:pointer; background: #009CDE;color:#FFF; border-radius:3px;">Trazar ruta</button>
-        </div>`;
-
-      data.nameEvent !== '' ? popupContent = introContent + eventContent + footerContent : popupContent = introContent + footerContent;
-
-      marker.bindPopup(popupContent);
-      this.markersRest.addLayer(marker);
+      resolve(); // Indicamos que la operación ha terminado
     });
 
-    this.markersRest.addTo(this.map);
+  }
+
+  async getMarkers() {
+
+    return new Promise<void>((resolve, reject) => {
+
+      this.mapService.getAllPlaces()
+        .subscribe({
+          next: (resp) => {
+            this.globalMarkers = resp;
+            resolve(); // Indicamos que la operación ha terminado
+          },
+          error: (err) => {
+            console.log(err);
+            reject(err); // Indicamos que ocurrió un error
+          }
+        });
+
+    });
+
+  }
+
+  async setMarkers() {
+    return new Promise<void>((resolve) => {
+      this.markersRest.clearLayers();
+
+      this.globalMarkers.forEach((data: PlaceData) => {
+        const lat = data.coordinates.lat;
+        const lng = data.coordinates.lng;
+        const iconResp = L.icon({
+          iconUrl: `./assets/imgs/map/${data.type}.png`,
+          iconSize: [32, 32],
+          iconAnchor: [16, 32],
+          popupAnchor: [0, -32]
+        });
+        const marker = L.marker([lat, lng], {icon: iconResp});
+        let popupContent = '';
+        const introContent = `
+          <h3 style="text-align: center; margin-bottom: 10px">${data.name}</h3>
+          <p style="text-align: justify; width: 100%;">${data.description}</p>
+          <div style="width: 100%; height:70px; display:flex; gap:5px; justify-content: center; align-items: center;">
+            <div style="width: 100px;">
+              <img style="width:100%" src="https://www.corazondepuebla.com.mx/wp-content/uploads/2018/10/header-chignahuapan-puebla-mexico-pueblos-magicos.jpg">
+            </div>
+            <div style="width: 100px;">
+              <img style="width:100%" src="https://mexicodesconocidoviajes.mx/wp-content/uploads/2018/09/Puebla-Chignahuapan-festival-luz-y-Vida-DPG_IMG_2006.jpg">
+            </div>
+          </div>`;
+
+          const eventContent = `
+          <div style="margin:10px 0px;">
+            <p><span style="font-weight: bold;">Evento:</span> ${data.nameEvent}</p>
+            <p><span style="font-weight: bold;">Fecha: </span>${data.dayEvent} <span style="font-weight: bold;">Hora: </span>${data.hourEvent}</p>
+          </div>`;
+
+          const footerContent = `
+          <div style="display:flex; gap:5px; justify-content: center; align-items: center; margin-bottom:10px; margin-top:10px;">
+            <button style="width:auto; padding: 5px 10px; cursor:pointer; background: #009CDE;color:#FFF; border-radius:3px;">Ver lugar</button>
+            <button style="width:auto; padding: 5px 10px; cursor:pointer; background: #009CDE;color:#FFF; border-radius:3px;">Trazar ruta</button>
+          </div>`;
+
+        data.nameEvent !== '' ? popupContent = introContent + eventContent + footerContent : popupContent = introContent + footerContent;
+
+        marker.bindPopup(popupContent);
+        this.markersRest.addLayer(marker);
+      });
+
+      this.markersRest.addTo(this.map);
+      resolve(); // Indicamos que la operación ha terminado
+
+    })
+
   }
 
 }
